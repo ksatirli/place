d3fee4bcf114de999a75e12ab91ee415c08b41ba
Kerim Satirli <k.satirli@synapticate.com>
2019-12-20 13:15:54 +0100

    adds config variables

---
+++ make/CONFIG.mk
@@ -0,0 +1,22 @@
+ansible-binary ?= ansible
+ansible-version ?= 2.9.2
+ansible-verbosity ?=
+
+brew-base-url ?= "https://raw.githubusercontent.com/Homebrew/install/master"
+brew-installer-url ?= $(brew-base-url)/install
+brew-uninstaller-url ?= $(brew-base-url)/uninstall
+brew-installer-file ?= "brew-installer-$(timestamp).rb"
+brew-uninstaller-file ?= "brew-uninstaller-$(timestamp).rb"
+
+color-off = \033[0m
+color-mute = \033[0;37m
+color-bright = \033[1;37m
+
+pip-binary ?= pip3
+
+place-directory ?= $(HOME)/.config/my-place
+place-sign = "📍 "
+
+timestamp := $(shell date +'%Y%m%d_%H%M')
+
+user ?= $(USER)
