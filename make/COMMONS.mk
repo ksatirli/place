676863a6f6973d844ff5c8620e8ad679fe925777
Kerim Satirli <k.satirli@synapticate.com>
2019-12-20 13:15:43 +0100

    adds targets for overview and creation of config directory

--- make/COMMONS.mk
+++ make/COMMONS.mk
@@ -16,3 +16,38 @@ help: overview # displays this help text
 	awk 'BEGIN {FS = ":.*?# "}; {printf "  \033[1m  %s\033[0m;%s\n", $$1, $$2}' | \
 	column -c2 -s ";" -t
 	@echo
+
+.PHONY: overview
+overview:
+	@echo
+	@echo -e "$(color-bright)$(place-sign) Place.$(color-mute)"
+	@echo
+	@echo
+	@echo	-e "$(color-bright)CONFIGURATION$(color-off)"
+	@echo -e "    $(color-bright)user:$(color-off)              $(user)"
+	@echo -e "    $(color-bright)home:$(color-off)              $(place-directory)"
+
+ifdef only-tags
+	@echo -e "    $(color-bright)only-tags:$(color-off)         $(only-tags)"
+endif
+
+ifdef skip-tags
+	@echo -e "    $(color-bright)skip-tags:$(color-off) 	       $(skip-tags)"
+endif
+
+ifdef verbosity
+	@echo -e "    $(color-bright)verbosity:$(color-off) 	      $(verbosity)"
+endif
+
+ifdef ansible-binary
+	@echo -e "    $(color-bright)ansible-binary:$(color-off)    $(ansible-binary)"
+endif
+
+ifdef pip-binary
+	@echo -e "    $(color-bright)pip-binary:$(color-off)        $(pip-binary)"
+endif
+
+create-config-directory:
+# In most runs, this target will not actually have to create $(place-directory),
+# as the target would be executed, in a normal flow, with the very first run.
+	@mkdir -p "$(place-directory)"
