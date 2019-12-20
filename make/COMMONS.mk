f1c0e8b3b835e697432b2d141fbbfe2780e7c42c
Kerim Satirli <k.satirli@synapticate.com>
2019-12-20 13:15:20 +0100

    adds target for help

--- make/COMMONS.mk
+++ make/COMMONS.mk
@@ -1,3 +1,18 @@
 .SILENT .PHONY: clear
 clear:
 	clear
+
+.SILENT .PHONY: help
+help: overview # displays this help text
+	@echo
+	@echo -e	"$(color-bright)OPTIONS$(color-off)"
+
+	grep \
+		--context=0 \
+		--devices=skip \
+		--extended-regexp \
+		--no-filename \
+			"(^[a-z-]+){1}: ?(?:[a-z-])* #" $(MAKEFILE_LIST) | \
+	awk 'BEGIN {FS = ":.*?# "}; {printf "  \033[1m  %s\033[0m;%s\n", $$1, $$2}' | \
+	column -c2 -s ";" -t
+	@echo
