65b4e971aed2aa82031b419a882edbf9707d0a94
Kerim Satirli <k.satirli@synapticate.com>
2019-12-20 13:43:39 +0100

    adds targets for install and uninstall

---
+++ make/install.mk
@@ -0,0 +1,4 @@
+.PHONY install:
+install: overview xcode-install brew-install ansible-install # installs Xcode CLI Tools, Brew, and Ansible
+
+# TODO: the help text for this file does not show up yet
---
+++ make/uninstall.mk
@@ -0,0 +1,4 @@
+.PHONY uninstall:
+uninstall: overview ansible-uninstall brew-uninstall xcode-reset # uninstalls Ansible, and Brew and resets Xcode CLI Tools
+
+# TODO: the help text for this file does not show up yet
