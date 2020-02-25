228913d2c09cc528fb38579a9863fa41975695ae
Kerim Satirli <k.satirli@gmail.com>
2020-02-25 14:15:50 +0100

    adds target to install VS Code extensions

---
+++ make/vscode-install-extensions.mk
@@ -0,0 +1,19 @@
+.PHONY vscode-install-extensions:
+vscode-install-extensions: # installs Visual Studio Code Extensions
+ifeq ($(shell which code 2>/dev/null 2>&1; echo $$?), 1)
+	$(info $(sign-warning)  code is not available in $$PATH)
+	$(info Consider installing Visual Studio Code)
+	@exit 1
+else ifeq ("$(wildcard $(vscodefile))","")
+	$(info $(sign-warning)  No vscodefile available at $(vscodefile))
+	@exit 1
+else
+	@echo
+	@echo "$(sign-place)  Installing Visual Studio Code Extensions"
+	@echo
+	@$(foreach extension, $(shell cat "$(vscodefile)"), \
+		code \
+			--force \
+			--install-extension "$(extension)" \
+	)
+endif
